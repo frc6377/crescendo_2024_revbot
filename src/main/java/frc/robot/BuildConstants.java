7010efbf860997d32ade4110cce48251b1cee4c7
RobbyV2 <robby@robby.blue>
2024-01-21 16:03:11 -0600

    Init

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "crescendo_2024_revbot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "48e3cd756b4a69ba13ff91625fc017efb0fdf4f7";
+  public static final String GIT_DATE = "2024-01-21 15:48:39 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-21 16:01:58 CST";
+  public static final long BUILD_UNIX_TIME = 1705874518155L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
