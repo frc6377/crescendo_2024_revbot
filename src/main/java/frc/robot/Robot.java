edfedf83ab3ec8d520169485d4bc69bbd9d22e36
RobbyV2 <robby@robby.blue>
2024-01-28 18:15:54 -0600

    Spotless

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OperatorConstants;
-
 import org.littletonrobotics.junction.LoggedRobot;
 
 /**
