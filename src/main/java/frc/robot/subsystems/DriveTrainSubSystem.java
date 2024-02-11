9ef5852869f6157a5b2fe02f1ff67c2392f24ca8
RobbyV2 <robby@robby.blue>
2024-02-11 17:15:21 -0600

    Make Drivebase Subsystem (#12)
    * Make Drivebase Subsystem
    Some files that were changed seemingly have no visible changes, that were only change on spotlessApply?
    
    * Remove redundant code

---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubSystem.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveTrainSubSystem extends SubsystemBase {
+  private final VictorSPX m_leftMotor = new VictorSPX(DriveConstants.kLeftMotorPort);
+  private final VictorSPX m_leftMotor2 = new VictorSPX(DriveConstants.kLeftMotorPort2);
+  private final VictorSPX m_rightMotor = new VictorSPX(DriveConstants.kRightMotorPort);
+  private final VictorSPX m_rightMotor2 = new VictorSPX(DriveConstants.kRightMotorPort2);
+  private DifferentialDrive m_robotDrive;
+
+  public DriveTrainSubSystem() {
+    m_leftMotor2.follow(m_leftMotor);
+    m_rightMotor2.follow(m_rightMotor);
+
+    SendableRegistry.addLW(this, "DriveTrainSubSystem");
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightMotor.setInverted(true);
+
+    m_robotDrive =
+        new DifferentialDrive(
+            speed -> m_leftMotor.set(ControlMode.PercentOutput, speed),
+            speed -> m_rightMotor.set(ControlMode.PercentOutput, speed));
+  }
+
+  public void arcadeDrive(double fwd, double rot) {
+    m_robotDrive.arcadeDrive(fwd, rot);
+  }
+
+  public void tankDrive(double lspeed, double rspeed) {
+    m_robotDrive.tankDrive(lspeed, rspeed);
+  }
+
+  public void setSpeeds(double leftSpeed, double rightSpeed) {
+    m_leftMotor.set(ControlMode.PercentOutput, leftSpeed);
+    m_rightMotor.set(ControlMode.PercentOutput, rightSpeed);
+  }
+
+  public void setBrakeMode() {
+    m_leftMotor.setNeutralMode(NeutralMode.Brake);
+    m_leftMotor2.setNeutralMode(NeutralMode.Brake);
+    m_rightMotor.setNeutralMode(NeutralMode.Brake);
+    m_rightMotor2.setNeutralMode(NeutralMode.Brake);
+  }
+
+  public void setCoastMode() {
+    m_leftMotor.setNeutralMode(NeutralMode.Coast);
+    m_leftMotor2.setNeutralMode(NeutralMode.Coast);
+    m_rightMotor.setNeutralMode(NeutralMode.Coast);
+    m_rightMotor2.setNeutralMode(NeutralMode.Coast);
+  }
+}
