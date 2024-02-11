d3e3a1f536445f1e2b42ba824d94951dca759616
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2024-02-11 17:03:55 -0600

    change to correct motor

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,8 +9,10 @@ import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private final Spark m_intakeMotor = new Spark(DriveConstants.intakeMotor);
-  private final Spark m_intakeMotor2 = new Spark(DriveConstants.intakeMotor2);
+  private final CANSparkMax m_intakeMotor =
+      new CANSparkMax(DriveConstants.intakeMotor, MotorType.kBrushless);
+  private final CANSparkMax m_intakeMotor2 =
+      new CANSparkMax(DriveConstants.intakeMotor2, MotorType.kBrushless);
 
   public void runIntake() {
     m_intakeMotor.set(Constants.intakeConstants.firstMotorPercent);
