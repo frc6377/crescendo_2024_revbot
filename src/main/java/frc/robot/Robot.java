7c5f7e103ec9ac9c433ce28c947c65b938e22592
RobbyV2 <robby@robby.blue>
2024-03-21 21:07:12 -0500

    Fix Logging and Drivetrain Also updated WPILib version, and vendor libraries, along with having an actual command scheduler.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.DriveTrainSubSystem;
 import org.littletonrobotics.junction.LoggedRobot;
@@ -14,10 +16,14 @@ public class Robot extends LoggedRobot {
   private XboxController m_DriverJoystick;
   private final DriveTrainSubSystem m_driveTrain = new DriveTrainSubSystem();
 
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
+
   @Override
   public void robotInit() {
     m_DriverJoystick = new XboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
     m_driveTrain.setBrakeMode();
+    m_robotContainer = new RobotContainer();
   }
 
   public void teleopPeriodic() {
@@ -25,4 +31,75 @@ public class Robot extends LoggedRobot {
     double rotation = m_DriverJoystick.getRightX();
     m_driveTrain.arcadeDrive(forward, rotation);
   }
+
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
